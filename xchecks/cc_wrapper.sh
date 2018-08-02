1a97608e94ebb191ea301b1c7563bbf0b714d6a2
Andrei Homescu <ah@immunant.com>
2018-08-01 18:18:33 -0700

    Added everything needed to run the C version with cross-checks

---
+++ xchecks/cc_wrapper.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#
+# Compiler wrapper script for cross-checks
+# Pass to autogen.sh when running it:
+# $ ./autogen.sh CC=./xchecks/cc_wrapper.sh [other autogen arguments...]
+
+SCRIPT_DIR=`dirname $0`
+C2RUST_DIR=`readlink -f $SCRIPT_DIR/../../../..`
+MACHINE=`uname -n`
+CC_WRAPPER="$C2RUST_DIR/cross-checks/c-checks/clang-plugin/cc_wrapper.sh"
+CC="$C2RUST_DIR/dependencies/llvm-6.0.1/build.$MACHINE/bin/clang"
+XCHECK_PLUGIN="$C2RUST_DIR/dependencies/clang-xcheck-plugin.$MACHINE/plugin/CrossChecks.so"
+XCHECK_RUNTIME="$C2RUST_DIR/dependencies/clang-xcheck-plugin.$MACHINE/runtime/libruntime.a"
+FAKECHECKS_PATH="$C2RUST_DIR/cross-checks/libfakechecks"
+
+exec "$CC_WRAPPER" "$CC" "$XCHECK_PLUGIN" "$XCHECK_RUNTIME" \
+    -ffunction-sections \
+    "$@" \
+    -L$FAKECHECKS_PATH -lfakechecks -lpthread -Wl,-rpath,$FAKECHECKS_PATH \
+    -Xclang -plugin-arg-crosschecks -Xclang -C$SCRIPT_DIR/libxml2_c.yaml
---
+++ xchecks/xchecks.c
@@ -0,0 +1,19 @@
+#include <stdint.h>
+#include <stddef.h>
+
+uint64_t __c2rust_hash_internal_state_struct(void *x, size_t depth) {
+    return 0xABCD0001;
+}
+
+uint64_t __c2rust_hash_lzma_internal_s_struct(void *x, size_t depth) {
+    return 0xABCD0002;
+}
+
+uint64_t xcheck_hash_xmlSchemaValDate(void *x, size_t depth) {
+    return 0xABCD0003;
+}
+
+uint64_t xcheck_hash_xmlSchemaValDecimal(void *x, size_t depth) {
+    return 0xABCD0004;
+}
+
