672746f6dd20d6edd0fede710fce0bbb2b1fcbc4
Andrei Homescu <ah@immunant.com>
2018-08-06 16:46:59 -0700

    Switch the clang build to use fakechecks-zstd library

--- xchecks/cc_wrapper.sh
+++ xchecks/cc_wrapper.sh
@@ -11,10 +11,10 @@ CC_WRAPPER="$C2RUST_DIR/cross-checks/c-checks/clang-plugin/cc_wrapper.sh"
 CC="$C2RUST_DIR/dependencies/llvm-6.0.1/build.$MACHINE/bin/clang"
 XCHECK_PLUGIN="$C2RUST_DIR/dependencies/clang-xcheck-plugin.$MACHINE/plugin/CrossChecks.so"
 XCHECK_RUNTIME="$C2RUST_DIR/dependencies/clang-xcheck-plugin.$MACHINE/runtime/libruntime.a"
-FAKECHECKS_PATH="$C2RUST_DIR/cross-checks/libfakechecks"
+FAKECHECKS_PATH="$C2RUST_DIR/cross-checks/rust-checks/target/release"
 
 exec "$CC_WRAPPER" "$CC" "$XCHECK_PLUGIN" "$XCHECK_RUNTIME" \
     -ffunction-sections \
     "$@" \
-    -L$FAKECHECKS_PATH -lfakechecks -lpthread -Wl,-rpath,$FAKECHECKS_PATH \
+    -L$FAKECHECKS_PATH -lfakechecks_zstd -lpthread -Wl,-rpath,$FAKECHECKS_PATH \
     -Xclang -plugin-arg-crosschecks -Xclang -C$SCRIPT_DIR/libxml2_c.yaml
