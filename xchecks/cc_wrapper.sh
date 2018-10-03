092c7c0db1bf777d77d43db1bc68cf3067b3a0c1
Andrei Homescu <ah@immunant.com>
2018-10-02 19:37:31 -0700

    Updated cc_wrapper.sh script for the MVEE

--- xchecks/cc_wrapper.sh
+++ xchecks/cc_wrapper.sh
@@ -11,11 +11,26 @@ CC_WRAPPER="$C2RUST_DIR/cross-checks/c-checks/clang-plugin/cc_wrapper.sh"
 CC="$C2RUST_DIR/dependencies/llvm-6.0.1/build.$MACHINE/bin/clang"
 XCHECK_PLUGIN="$C2RUST_DIR/dependencies/clang-xcheck-plugin.$MACHINE/plugin/CrossChecks.so"
 XCHECK_RUNTIME="$C2RUST_DIR/dependencies/clang-xcheck-plugin.$MACHINE/runtime/libruntime.a"
-#FAKECHECKS_PATH="$C2RUST_DIR/cross-checks/libfakechecks"
-FAKECHECKS_PATH="$C2RUST_DIR/cross-checks/rust-checks/target/release"
+
+#XCHECK_LIB=fakechecks
+#XCHECK_LIB_PATH="$C2RUST_DIR/cross-checks/libfakechecks"
+#XCHECK_LIB=fakechecks_zstd
+#XCHECK_LIB_PATH="$C2RUST_DIR/cross-checks/rust-checks/target/release"
+XCHECK_LIB=clevrbuf
+XCHECK_LIB_PATH="$C2RUST_DIR/cross-checks/ReMon/libclevrbuf"
+
+OUR_CFLAGS="-ffunction-sections"
+
+# The C version calls stat64() and the Rust one calls stat()
+# without this, which leads to divergence
+OUR_CFLAGS+=" -DNO_LARGEFILE_SOURCE"
+
+# We need `-fno-builtin` for testlimits.c, since it
+# calls `printf` with constant strings, and we don't want
+# clang to convert those to `puts`
+OUR_CFLAGS+=" -fno-builtin"
 
 exec "$CC_WRAPPER" "$CC" "$XCHECK_PLUGIN" "$XCHECK_RUNTIME" \
-    -ffunction-sections \
-    "$@" \
-    -L$FAKECHECKS_PATH -lfakechecks_zstd -lpthread -Wl,-rpath,$FAKECHECKS_PATH \
+    $OUR_CFLAGS "$@" \
+    -L$XCHECK_LIB_PATH -l$XCHECK_LIB -lpthread \
     -Xclang -plugin-arg-crosschecks -Xclang -C$SCRIPT_DIR/libxml2_c.yaml
